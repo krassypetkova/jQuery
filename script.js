b53bb55b449a166e9136e69d656bdef41b8763cf
krassypetkova <krassypetkova@gmail.com>
2015-12-01 22:46:20 +0200

    Green/red moon application 1
    -html file
    -css file
    jQuery file

---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+   /* $('div').click(function() {
+        $(this).hover(function() {
+            $(this).addClass('red');
+        })
+    });*/
+    $( "div" ).click(function( event ) {
+
+        alert( "Thanks for visiting!" );
+
+    });
+});
