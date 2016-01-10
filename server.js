f6ccb9a80d0d2bbe49e42915ac70733962b9cfad
krassypetkova <krassypetkova@gmail.com>
2016-01-10 10:30:51 +0200

    Client and server CRUD operations implemented

--- server.js
+++ server.js
@@ -37,6 +37,12 @@ app.get('/api/todo', function (req, res) {
     res.json(todos);
 });
 
+app.get('/api/todo/:todo_id', function (req, res) {
+    var todoId = req.params.todo_id;
+
+    res.json(todos[todoId]);
+});
+
 app.post('/api/todo', function (req, res) {
     var todoTitle = req.body.title,
         todoCompleted = JSON.parse(req.body.completed),
@@ -46,6 +52,23 @@ app.post('/api/todo', function (req, res) {
 
     counterTodos++;
    res.json(newTodo);
+});
+
+app.put('/api/todo/:todo_id', function (req, res) {
+    var reqTodoId = req.params.todo_id,
+        todoId = req.body.id,
+        todoTitle = req.body.title,
+        todoCompleted = req.body.completed,
+        objTodo = new Todo(todoId, todoTitle, todoCompleted);
+
+    todos[reqTodoId] = objTodo;
+    res.json(todos[reqTodoId]);
+});
+
+app.delete('/api/todo/:todo_id', function (req, res) {
+    var reqTodoId = req.params.todo_id;
+    delete todos[reqTodoId];
+    res.send(200);
 });
 
 //how to get data from request
